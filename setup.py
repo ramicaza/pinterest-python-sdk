4a0098f50d6566466c87688e84a3b9e5dc220d85
Nico <ramicaza@gmail.com>
2024-09-20 01:08:59 -0400

    loosen reqs

--- setup.py
+++ setup.py
@@ -21,11 +21,11 @@ def _get_prod_version():
 _IS_TEST_BUILD = os.environ.get("IS_TEST_BUILD", 0)
 
 REQUIRES = [
-  "urllib3==1.26.12",
+  "urllib3>=1.26.12,<3",
   "python-dateutil",
   "python-dotenv==0.20.0",
   "six==1.16.0",
-  "Pinterest-Generated-Client==0.1.9"
+  "Pinterest-Generated-Client @ git+https://github.com/ramicaza/pinterest-python-generated-api-client"
 ]
 
 long_description = (Path(__file__).parent / "README.md").read_text()
